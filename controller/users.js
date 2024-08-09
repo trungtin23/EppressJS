f8ce102e732b4145c1a3d986afb8986fba05de29
tin <21130564@st.hcmuaf.edu.vn>
2024-08-09 13:35:15 +0700

    update format return restAPI

--- controller/users.js
+++ controller/users.js
@@ -1,55 +1,95 @@
 const { v4: uuidv4 } = require('uuid');
 
-let users = []
+let users = [];
 
 const getUser = (req, res) => {
-    console.log(users);
-    res.send(users);
+    if (users.length === 0) {
+        res.status(404).json({
+            status: "error",
+            message: "No users found",
+            errors: [{ message: "The users list is empty" }]
+        });
+    } else {
+        res.status(200).json({
+            status: "success",
+            data: users,
+            message: "Users retrieved successfully"
+        });
+    }
 };
 
-const addUser =(req,res) =>{
-    const user  = req.body
+const addUser = (req, res) => {
+    const user = req.body;
+    const newUser = { ...user, id: uuidv4() };
+    users.push(newUser);
 
-    users.push({...user, id: uuidv4()})
+    res.status(201).json({
+        status: "success",
+        data: newUser,
+        message: `User ${newUser.firstName} added`
+    });
+};
 
-    res.send(`USER ${user.firstName} added`)
+const findUser = (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((user) => user.id === id);
 
-}
+    if (foundUser) {
+        res.status(200).json({
+            status: "success",
+            data: foundUser,
+            message: "User found"
+        });
+    } else {
+        res.status(404).json({
+            status: "error",
+            message: `User with id ${id} not found`,
+            errors: [{ field: "id", message: "User not found" }]
+        });
+    }
+};
 
-const findUser = (req,res) =>{
-    const {id} = req.params
+const deleteUser = (req, res) => {
+    const { id } = req.params;
+    const initialLength = users.length;
+    users = users.filter((user) => user.id !== id);
 
-    const foudUser = users.find((user) => user.id == id) 
+    if (users.length < initialLength) {
+        res.status(200).json({
+            status: "success",
+            message: `Deleted user with id ${id}`
+        });
+    } else {
+        res.status(404).json({
+            status: "error",
+            message: `User with id ${id} not found`,
+            errors: [{ field: "id", message: "User not found" }]
+        });
+    }
+};
 
-    res.send(foudUser)
+const updateUser = (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+    const user = users.find((user) => user.id === id);
 
-}
+    if (user) {
+        if (firstName) user.firstName = firstName;
+        if (lastName) user.lastName = lastName;
+        if (age) user.age = age;
 
-const deleteUser =(req,res) =>{
-    const {id} = req.params 
-    users = users.filter((user) => user.id !== id) 
+        res.status(200).json({
+            status: "success",
+            data: user,
+            message: `User with id ${id} has been updated`
+        });
+    } else {
+        res.status(404).json({
+            status: "error",
+            message: `User with id ${id} not found`,
+            errors: [{ field: "id", message: "User not found" }]
+        });
+    }
+};
 
-    res.send(`deleted user has id ${id}`)
-  
-
-}
-
-const updateUser =  (req,res) =>{
-    const {id} = req.params 
-    const {firstName,lastName,age}  = req.body
-
-    const user = users.find((user) => user.id == id) 
-
-    if(firstName){user.firstName = firstName}
-
-    if(lastName){user.lastName = lastName}
-
-    if(age){user.age = age}
-
-    
-    res.send(`user has id: ${id} has been updated`)
-  
-
-}
-
-module.exports = { getUser,addUser,findUser,deleteUser,updateUser };
+module.exports = { getUser, addUser, findUser, deleteUser, updateUser };
